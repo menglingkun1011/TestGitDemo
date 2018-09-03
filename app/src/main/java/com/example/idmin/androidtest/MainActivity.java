abf42b24b053c568215fad624dc22315aba4b13c
孟令坤 <menglingkun_vip@126.com>
2018-09-03 15:06:32 +0800

    第二次修改并提交

--- app/src/main/java/com/example/idmin/androidtest/MainActivity.java
+++ app/src/main/java/com/example/idmin/androidtest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //first git提交
+        //第二次修改  并提交
     }
 }
