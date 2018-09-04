c0c4a8e9dd0a2c10eb3146c8c1952cf973a25792
孟令坤 <menglingkun_vip@126.com>
2018-09-04 09:39:50 +0800

    。。

---
+++ app/src/main/java/com/example/idmin/androidtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.idmin.androidtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //first git提交
+        //第二次修改  并提交
+        //第三次
+        //第四次
+        //修复bug
+
+    }
+}
